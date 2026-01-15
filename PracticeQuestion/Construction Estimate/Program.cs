1b801df2e40c6d1ff87374ae0e00b00089ee59cf
Mukesh Kumar <121216988+Mukesh-Swami-0612@users.noreply.github.com>
2026-01-15 18:10:56 +0530

    Add files via upload

---
+++ PracticeQuestion/Construction Estimate/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+public class EstimateDetails
+{
+    public float ConstructionArea { get; set; }
+    public float SiteArea { get; set; }
+}
+
+public class ConstructionEstimateException : Exception
+{
+    public ConstructionEstimateException() : base("Sorry your Construction Estimate is not approved")
+    {
+    }
+}
+
+public class Program
+{
+    public static EstimateDetails ValidateConstructionEstimate(float constructionArea, float siteArea)
+    {
+        if (constructionArea <= siteArea)
+        {
+            return new EstimateDetails
+            {
+                ConstructionArea = constructionArea,
+                SiteArea = siteArea
+            };
+        }
+        else
+        {
+            throw new ConstructionEstimateException();
+        }
+    }
+
+    public static void Main(string[] args)
+    {
+        float constructionArea = Convert.ToSingle(Console.ReadLine());
+        float siteArea = Convert.ToSingle(Console.ReadLine());
+
+        try
+        {
+            EstimateDetails result = ValidateConstructionEstimate(constructionArea, siteArea);
+            Console.WriteLine("Construction Estimate Approved");
+        }
+        catch (ConstructionEstimateException ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+    }
+}
