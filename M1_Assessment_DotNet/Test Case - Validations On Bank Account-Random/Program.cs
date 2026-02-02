fecb5c71a07cfcf974b4a1c3aa2261893135aebd
Mukesh Kumar <121216988+Mukesh-Swami-0612@users.noreply.github.com>
2026-02-02 14:50:50 +0530

    Update Program.cs

--- M1_Assessment_DotNet/Test Case - Validations On Bank Account-Random/Program.cs
+++ M1_Assessment_DotNet/Test Case - Validations On Bank Account-Random/Program.cs
@@ -1,1 +1,63 @@
+using NUnit.Framework;
+using System;
 
+namespace BankAccountTests
+{
+    [TestFixture] // Required attribute for test class
+    public class UnitTest
+    {
+        // Test for valid deposit
+        [Test]
+        public void Test_Deposit_ValidAmount()
+        {
+            // Arrange
+            Program account = new Program(1000m);
+
+            // Act
+            account.Deposit(500m);
+
+            // Assert
+            Assert.AreEqual(1500m, account.Balance);
+        }
+
+        // Test for negative deposit
+        [Test]
+        public void Test_Deposit_NegativeAmount()
+        {
+            // Arrange
+            Program account = new Program(1000m);
+
+            // Act & Assert
+            var ex = Assert.Throws<Exception>(() => account.Deposit(-200m));
+
+            Assert.AreEqual("Deposit amount cannot be negative", ex.Message);
+        }
+
+        // Test for valid withdrawal
+        [Test]
+        public void Test_Withdraw_ValidAmount()
+        {
+            // Arrange
+            Program account = new Program(1000m);
+
+            // Act
+            account.Withdraw(400m);
+
+            // Assert
+            Assert.AreEqual(600m, account.Balance);
+        }
+
+        // Test for insufficient funds
+        [Test]
+        public void Test_Withdraw_InsufficientFunds()
+        {
+            // Arrange
+            Program account = new Program(500m);
+
+            // Act & Assert
+            var ex = Assert.Throws<Exception>(() => account.Withdraw(800m));
+
+            Assert.AreEqual("Insufficient funds.", ex.Message);
+        }
+    }
+}
