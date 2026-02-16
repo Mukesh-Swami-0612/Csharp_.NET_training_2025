7cad7a6a7e7411460c153ba52fcf2319c856545f
Mukesh Kumar <121216988+Mukesh-Swami-0612@users.noreply.github.com>
2026-02-16 20:12:26 +0530

    Create Library Book Management System.cs

---
+++ Practice_16_Feb/C# Collections & Generics/Library Book Management System.cs
@@ -0,0 +1,90 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Book
+{
+    public string ISBN { get; set; }
+    public string Title { get; set; }
+    public string Author { get; set; }
+    public string Genre { get; set; }
+    public bool IsAvailable { get; set; } = true;
+}
+
+// Generic Catalog Class
+public class Catalog<T> where T : Book
+{
+    private List<T> _items = new List<T>();
+    private HashSet<string> _isbnSet = new HashSet<string>();
+    private SortedDictionary<string, List<T>> _genreIndex =
+        new SortedDictionary<string, List<T>>();
+
+    // Add item with genre indexing
+    public bool AddItem(T item)
+    {
+        if (item == null)
+            return false;
+
+        // Check ISBN uniqueness
+        if (_isbnSet.Contains(item.ISBN))
+        {
+            Console.WriteLine("Duplicate ISBN not allowed.");
+            return false;
+        }
+
+        // Add to main list
+        _items.Add(item);
+        _isbnSet.Add(item.ISBN);
+
+        // Add to genre index
+        if (!_genreIndex.ContainsKey(item.Genre))
+            _genreIndex[item.Genre] = new List<T>();
+
+        _genreIndex[item.Genre].Add(item);
+
+        Console.WriteLine("Book added successfully.");
+        return true;
+    }
+
+    // Indexer to get books by genre
+    public List<T> this[string genre]
+    {
+        get
+        {
+            if (_genreIndex.ContainsKey(genre))
+                return _genreIndex[genre];
+
+            return new List<T>();
+        }
+    }
+
+    // Find books using LINQ and lambda
+    public IEnumerable<T> FindBooks(Func<T, bool> predicate)
+    {
+        return _items.Where(predicate);
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Catalog<Book> library = new Catalog<Book>();
+
+        Book book1 = new Book
+        {
+            ISBN = "978-3-16-148410-0",
+            Title = "C# Programming",
+            Author = "John Sharp",
+            Genre = "Programming"
+        };
+
+        library.AddItem(book1);
+
+        var programmingBooks = library["Programming"];
+        Console.WriteLine(programmingBooks.Count); 
+
+        var johnsBooks = library.FindBooks(b => b.Author.Contains("John"));
+        Console.WriteLine(johnsBooks.Count());      
+    }
+}
